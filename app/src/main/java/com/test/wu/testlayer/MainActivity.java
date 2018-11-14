629500e9979d8739955867f4fe82ab94c1ad3e3d
wuhongbo <wuhongbo2014@xiaochuankeji.cn>
2018-11-14 12:12:38 +0800

    hello-world测试安卓项目

---
+++ app/src/main/java/com/test/wu/testlayer/MainActivity.java
@@ -0,0 +1,17 @@
+package com.test.wu.testlayer;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+import com.test.wu.foundation.Plus;
+
+public class MainActivity extends Activity {
+
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView textView = (TextView) findViewById(R.id.test_num);
+        int n = Plus.plusTheNum();
+        textView.setText(n+"");
+    }
+}
---
+++ foundation_base/foundation/src/main/java/com/test/wu/foundation/Plus.java
@@ -0,0 +1,12 @@
+package com.test.wu.foundation;
+
+/**
+ * Created by wuhongbo on 2018/11/12.
+ */
+
+public class Plus {
+
+    public static int plusTheNum(){
+        return  5;
+    }
+}
