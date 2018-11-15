a6259627480ee476bb3b93d75f110d6e2c5e2d6c
wuhongbo <wuhongbo2014@xiaochuankeji.cn>
2018-11-15 17:47:52 +0800

    替换到json解析

--- app/src/main/java/com/test/wu/testlayer/MainActivity.java
+++ app/src/main/java/com/test/wu/testlayer/MainActivity.java
@@ -3,6 +3,7 @@ package com.test.wu.testlayer;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
+import com.izuiyou.json.JSONUtils;
 //import com.test.wu.foundation.Plus;
 
 public class MainActivity extends Activity {
@@ -12,7 +13,7 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         TextView textView = (TextView) findViewById(R.id.test_num);
         //int n = Plus.plusTheNum();
-
+        JSONUtils.contains(null, null);
 
 
 
